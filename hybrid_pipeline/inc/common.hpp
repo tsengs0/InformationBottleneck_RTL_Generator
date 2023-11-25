26513e159d70cce58c8b2e1cfae82ebf4c3157f5
tsengs0 <tsengs0@gmail.com>
2023-11-25 23:25:22 +0900

    Finalised the simulation of worst-case solution

--- hybrid_pipeline/inc/common.hpp
+++ hybrid_pipeline/inc/common.hpp
@@ -4,7 +4,9 @@
 #define SCHED_DEBUG_MODE
 #define SHIFT_DEBUG_MODE
 //#define WORST_SOL_DEBUG_MODE
+#define UNIT_TEST_MODE
 
+#define QUAN_SIZE 3
 #define GP2_NUM 2
 #define SHARE_GP_NUM 5
 
--- hybrid_pipeline/inc/shift_ctrl.hpp
+++ hybrid_pipeline/inc/shift_ctrl.hpp
@@ -11,6 +11,8 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <bitset>
+#include <ctime>
 
 #include "../inc/common.hpp"
 
@@ -60,13 +62,34 @@ typedef struct skid_buffer {
     unsigned short *col_addr;
 } skid_buffer_t;
 //==============================================================
-//
+// Message-pase buffer
 //==============================================================
+typedef unsigned int MSGPASS_BUFFER_RADDR;
+typedef unsigned short COL_ADDR;
+typedef struct msgPass_buffer_page {
+    COL_ADDR shiftCtrl_colAddr_buf2shift[SHARE_GP_NUM]; // tentative
+} msgPass_buffer_page_t;
+//==============================================================
+// L1PA shift-control register file
+//==============================================================
+typedef unsigned short RQST_FLAG;
+typedef unsigned short REGFILE_RADDR;
+typedef short SHIFT_SIG;
+typedef short SHIFT_DELTA_SIG;
+#define MIN_REGFILE_RADDR 0
+#define MAX_REGFILE_RADDR (((REGFILE_RADDR) 0x1 << SHARE_GP_NUM)-1)
 typedef struct shiftCtrl_regfile {
-    short l1pa_shift;
-    short l1pa_delta;
+    SHIFT_SIG l1pa_shift;
+    SHIFT_DELTA_SIG l1pa_delta;
     bool isGtr;
 } shiftCtrl_regfile_page;
+//==============================================================
+// For exporting log file
+//==============================================================
+typedef struct shiftCtrlUnit_outputData {
+    SHIFT_SIG l1pa_shift;
+    bool isGtr;
+} shiftCtrlUnit_outputData_t;
 //==============================================================
 //
 //==============================================================
@@ -75,38 +98,58 @@ typedef struct regfile_inteface {
     shiftCtrl_regfile_page page_out;
 
     // Input
-    unsigned short raddr_i;
+    REGFILE_RADDR raddr_i;
 } regFile_IF_t;
 //==============================================================
 //
 //==============================================================
-typedef struct pipeline_register_bank {
-    shiftCtrl_state fsm_state[PIPELINE_STAGE_NUM];
-} pipeline_regBank_t;
+typedef struct pipeline_reg {
+    // Pipeline stage 0
+    COL_ADDR col_addr_ff[SHARE_GP_NUM];
+    // Pipeline stage 1
+    shiftCtrl_regfile_page regfile_page_ff;
+    SHIFT_DELTA_SIG shiftDelta_interFF;
+    // Pipeline stage 2
+    SHIFT_SIG l1pa_shift_ff;
+} pipeline_reg_t;
 //==============================================================
 //
 //==============================================================
 class shift_control_unit {
     private:
-        std::string fsm_state_header[5];
-        unsigned short *colBankID_gp2;
-        unsigned short col_addr[SHARE_GP_NUM]; // The lates address to be input to address-mapping unit
-        std::vector<shiftCtrl_regfile_page> shiftCtrl_regfile;
-        unsigned short rqst_gp2;
-        regFile_IF_t regFile_IF;
-        short shift_gen_out[2];
+        // Input of this simulator
+        MSGPASS_BUFFER_RADDR msgBuffer_raddr_vec[SHARE_GP_NUM];
 
-        // Pipeline resource
-        pipeline_regBank_t pipeline_regBank;
+        std::string fsm_state_header[5];
+        COL_ADDR *colBankID_gp2;
+        COL_ADDR col_addr[SHARE_GP_NUM]; // The lates address to be input to address-mapping unit
+        std::vector<shiftCtrl_regfile_page> shiftCtrl_regfile;
+        RQST_FLAG rqst_gp2;
+
+        // Output of shift control unit (after the last pipeline stage)
+        SHIFT_SIG l1pa_shift_out;
+        bool isGtr_out;
+
+        // For exporting the log file
+        std::vector<shiftCtrlUnit_outputData_t> shiftCtrlUnit_logData;
 
     public:
-        shift_control_unit(unsigned short *colBankID_gp2_config);
+        pipeline_reg_t shiftCtrl_pipeline_reg; // Pipeline resource
+        regFile_IF_t regFile_IF; // I/F of shift ctrl. register file (internal LUT)
+
+        shift_control_unit(COL_ADDR *colBankID_gp2_config);
         shiftCtrl_state fsm_update(bool is_active, shiftCtrl_state fsm_state, unsigned short resourceID);
-        void col_addr_receive(unsigned short *col_addr_in);
-        void col_addr_read();
+        void msgBuffer_raddr_in(MSGPASS_BUFFER_RADDR *raddr_vec_in);
+        void msgBuffer_read();
+        void rqst_flag_gen(COL_ADDR *col_addr_in);
+        bool regfile_read();
         void shift_gen();
-        short shift_out(unsigned short alloc_cycle_id);
-        void fsm_process(unsigned short fsm_state);
+        short shift_out(bool is_2nd_shiftOut);
+        void fsm_process(shiftCtrl_state fsm_state, bool is_2nd_shiftOut);
+
+        // For exporting the log file
+        void shiftOut_export();
+        void shiftOut_log_read();
 
         // For debugging
         void show_regfile();
